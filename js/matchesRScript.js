ce16e2d2ad059e6e1d3284cc69fed894fd1cb52c
YeosuUnited <yeosuunited@gmail.com>
2025-02-01 23:29:48 +0900

    Update matchesRScript.js

--- js/matchesRScript.js
+++ js/matchesRScript.js
@@ -774,4 +774,15 @@ window.onload = async function () {
         passwordInput.value = '';
         errorMessage.style.display = 'none';
     }
+
+    const imgElements = document.querySelectorAll('img[data-image-name]');
+    for (const imgElement of imgElements) {
+        const imageUrl = `https://raw.githubusercontent.com/YeosuUnited/DataSite/main/assets/images/${imgElement.dataset.imageName}.png`;
+        try {
+            const blobUrl = await getCachedImageUrl(imageUrl);
+            imgElement.src = blobUrl;
+        } catch (error) {
+            imgElement.src = `https://raw.githubusercontent.com/YeosuUnited/DataSite/main/assets/images/default.png`;
+        }
+    }
 }
